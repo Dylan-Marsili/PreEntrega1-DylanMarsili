5bc2e9aaac869d2ca31d1571a118e7dd86b4c31d
= <=>
2023-06-13 19:39:47 -0300

    Modificaciones Sacar Imagenes que no sirven CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import ProfileCard from "./components/ProfileCard";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
-import AlexaImg from "./img/alexa.png";
-import CortanaImg from "./img/cortana.png";
-import SiriImg from "./img/siri.png";
 import "bulma/css/bulma.css";
 
 
@@ -16,13 +13,13 @@ function App(){
                 <section className="section">
                     <div className="columns">
                         <div className="column is-flex is-justify-content-center is-4">
-                            <ProfileCard titulo="Alexa" handle="@alexa88" img={AlexaImg} alt="Alexa"/>
+                            <ProfileCard title="Grand Theft Auto V" description="Grand Theft Auto V es un videojuego de acción-aventura de mundo abierto desarrollado por Rockstar North y publicado por Rockstar Games. Es el primer juego principal de la serie Grand Theft Auto desde Grand Theft Auto IV de 2008." img="https://th.bing.com/th/id/OIP.Uja6gdwBme6N_AId581P9gHaJF?pid=ImgDet&rs=1" alt="Alexa"/>
                         </div>
                         <div className="column is-flex is-justify-content-center is-4">
-                            <ProfileCard titulo="Siri" handle="@siri_99" img={SiriImg} alt="Siri"/>
+                            <ProfileCard title="Call of Duty: Modern Warfare" description="Call of Duty: Modern Warfare es un videojuego de disparos en primera persona desarrollado por Infinity Ward y publicado por Activision. Es el decimosexto juego de la serie Call of Duty y actúa como un reinicio suave de la subserie Modern Warfare." img="https://th.bing.com/th/id/OIP.Yz-uGutd0ueAg22hxaHZoQHaKX?pid=ImgDet&rs=1" alt="Siri"/>
                         </div>
                         <div className="column is-flex is-justify-content-center is-4">
-                            <ProfileCard titulo="Cortana" handle="@cortana_02" img={CortanaImg} alt="Cortana"/>            
+                            <ProfileCard title="Warframe" description="Warframe es un juego de disparos en tercera persona gratuito y cooperativo que se desarrolla en un mundo en constante expansión. Juega como uno de los Tenno, guerreros antiguos que han despertado de siglos de criostasis para encontrarse en una guerra con las facciones Grineer, Corpus y Infested." img="https://th.bing.com/th/id/R.3465c583ea184729c4baac5abd38e5bc?rik=cdoXEl1A95UDCw&pid=ImgRaw&r=0" alt="Cortana"/>            
                         </div>
                     </div>
                 </section>
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,7 +1,7 @@
 
 import {useState} from "react";
 
-function ProfileCard({titulo,img,handle, alt}){
+function ProfileCard({title,img,description, alt}){
 
     const [count, setCount] = useState(0);
 
@@ -18,22 +18,23 @@ function ProfileCard({titulo,img,handle, alt}){
         
         <div className="card">
             <div className="card-img">
-                <figure className="image">
-                    <img src={img} alt={alt}/>
+                <figure className="image is-3by4">
+                    <img src={img} alt={alt} height={100}/>
                 </figure>
             </div>
-            <div className="card-content">
+            <div className="card-content is-flex is-flex-direction-column">
                 <div className="media-content">
-                    <div>El titulo es {titulo} </div>
-                    <p>El handle es: {handle}</p>
-                    <div className="columns">
-                        <div className="column">
+                    <div className="title is-5">{title}</div>
+                    <p className="">{description}</p>
+                    <span className="title is-6 "><p className="pt-2 has-text-centered">Agrega al carrito:</p></span>
+                    <div className="columns pt-5">
+                        <div className="column is-flex is-justify-content-center">
                             <button className="button is-primary is-fullwidth" onClick={handleClickSuma}>+</button>
                         </div>
-                        <div className="column">
+                        <div className="column is-3 mt-1 is-flex is-justify-content-center">
                         <span><p className="content is-medium">{count}</p></span>
                         </div>
-                        <div className="column">
+                        <div className="column is-flex is-justify-content-center">
                             <button className="button is-danger is-fullwidth" onClick={handleClickResta}>-</button>
                         </div>
                     </div>
